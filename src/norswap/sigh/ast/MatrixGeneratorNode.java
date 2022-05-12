f4587f293132105de72bb54b7cb48ef2e5ad3e05
wiauxb <bastien.wiaux@student.uclouvain.be>
2022-05-12 12:16:05 +0200

    fixed to java 8 instead of 11

--- src/norswap/sigh/ast/MatrixGeneratorNode.java
+++ src/norswap/sigh/ast/MatrixGeneratorNode.java
@@ -1,8 +1,7 @@
 package norswap.sigh.ast;
 
 import norswap.autumn.positions.Span;
-import norswap.utils.Util;
-
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -20,18 +19,8 @@ public class MatrixGeneratorNode extends ExpressionNode
         this.filler = cast(filler, ExpressionNode.class);
         this.shape = cast(shape, List.class);
         if (this.shape.size() == 1){
-            this.shape = List.of(new ExpressionNode[]{new IntLiteralNode(this.shape.get(0).span, 1), this.shape.get(0)});
-        //.add(0, new IntLiteralNode(this.shape.get(0).span, 1));
+            this.shape = Arrays.asList(new IntLiteralNode(this.shape.get(0).span, 1), this.shape.get(0));
         }
-//        Object[] shapes = (Object[]) shape;
-//        if (shapes.length == 1){
-//            this.shape2 = Util.cast(shapes[0], ExpressionNode.class);
-//            this.shape1 = new IntLiteralNode(this.shape2.span, 1);
-//        }
-//        else {
-//            this.shape1 = Util.cast(shapes[0], ExpressionNode.class);
-//            this.shape2 = Util.cast(shapes[1], ExpressionNode.class);
-//        }
     }
 
     @Override public String contents () {
